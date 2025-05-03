07b894577d665967dfd72780f003c527ed9c1fbc
Caroline Ininga <carolineininga@gmail.com>
2025-05-03 15:52:22 +0300

    completed react components basics lab

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,15 @@ function Home() {
   );
 }
 
+function Component(){
+  return (
+    <div id = "about">
+
+      <h2>About</h2>
+    </div>
+  )
+}
+
 {/* write an <About> component here */}
 
 
@@ -23,8 +32,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <Component/>
     </div>
   );
 }
